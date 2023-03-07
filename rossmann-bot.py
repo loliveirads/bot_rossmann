d084c1f9a4f9ea740dd36543f7e75f0a5cd4de14
loliveirads <119435421+loliveirads@users.noreply.github.com>
2023-03-07 19:08:56 -0300

    Update rossmann-bot.py

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -26,112 +26,122 @@ TOKEN = '6256743741:AAGEWwfZMZIvDdwqhs444ly6hlj9PoOTbLM'
 # https://api.telegram.org/bot5869879507:AAGMhvcKGAh8vMOwt43CahFg-S95vH2qtIc/sendMessage?chat_id=1124066742&text=Hi Fabrício
 
 def send_message(chat_id, text):
+    url = 'https://api.telegram.org/bot/'.format(TOKEN)
+    url = url + 'sendMessage?chat_id={}'.format(chat_id)
 
-    url = 'https://api.telegram.org/bot{}/'.format(TOKEN)
-    url = url + 'sendMessage?chat_id={}'.format(chat_id) 
-    
-    r = requests.post(url, json={'text':text})
+    r = requests.post(url, json={'text': text})
     print('Status Code {}'.format(r.status_code))
 
     return None
 
-def load_dataset(store_id):
-    df10 = pd.read_csv('test.csv')
+
+
+# loading test dataset
+def load_data(store_id):
+    df10 = pd.read_csv( 'test.csv' )
     df_store_raw = pd.read_csv('store.csv')
 
+
     # merge test dataset + store
-    df_test = pd.merge(df10, df_store_raw, how='left', on='Store')
+    df_test = pd.merge( df10, df_store_raw, how='left', on='Store' )
 
     # choose store for prediction
-    df_test = df_test[df_test['Store'] == store_id ]
+    df_test = df_test[df_test['Store'] == store_id]
 
     if not df_test.empty:
-        # remove closed days
+         # remove closed days
         df_test = df_test[df_test['Open'] != 0]
         df_test = df_test[~df_test['Open'].isnull()]
-        df_test.drop('Id', axis=1, inplace=True)
+        df_test = df_test.drop( 'Id', axis=1 )
 
-        # convertendo dataframe para json
-        data = json.dumps( df_test.to_dict( orient = 'records' ))
-
+        # convert Dataframe to json
+        data = json.dumps( df_test.to_dict( orient='records' ) )
     else:
         data = 'error'
+    
 
     return data
 
 def predict(data):
-    # Chamada da API
+
+
+    # API Call
+    #url = 'http://192.168.1.11:5000/rossmann/predict'
     url = 'https://rossmann-predict-4nar.onrender.com/rossmann/predict'
-    header = {'Content-type': 'application/json'}
+    header = {'Content-type': 'application/json' } 
     data = data
 
-    r= requests.post(url, data=data, headers=header)
-    print('Status Code {}'.format(r.status_code))
+    r = requests.post( url, data=data, headers=header )
+    print( 'Status Code {}'.format( r.status_code ) )
 
-    d1 = pd.DataFrame(r.json(), columns=r.json()[0].keys())
-
+    d1 = pd.DataFrame( r.json(), columns=r.json()[0].keys() )
+    
     return d1
+
+
 
 def parse_message(message):
     chat_id = message['message']['chat']['id']
     store_id = message['message']['text']
-    store_id = store_id.replace('/','')
+
+    store_id.replace('/', '')
 
     try:
         store_id = int(store_id)
-    
+
     except ValueError:
         store_id = 'error'
 
+
     return chat_id, store_id
 
-# Iniciando API
+
+#API inicialize
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    
     if request.method == 'POST':
-        message = request.get_json()
+        message = request.get_jason()
 
         chat_id, store_id = parse_message(message)
 
         if store_id != 'error':
-            
-            # loading data
-            data = load_dataset(store_id)
+            #loading data
+            data = load_data(store_id)
 
             if data != 'error':
-               
-                # prediction
+
+                #prediction
                 d1 = predict(data)
-               
-                # calculation
-                d2 = d1[['store', 'prediction']].groupby('store').sum().reset_index()
-                
-                # send message
-                msg = "Store number {} will sell $ {:,.2f} in the next 6 weeks.".format(
-                    d2['store'].values[0],
-                    d2['prediction'].values[0])
-                
+
+                #calculation
+
+                d2 = d1[['store', 'prediction']].groupby( 'store' ).sum().reset_index()
+
+                #send message
+                msg =  'Store Number {} will sell R${:,.2f} in the next 6 weeks'.format( 
+                            d2['store'].values[0],
+                            d2['prediction'].values[0])
+
                 send_message(chat_id, msg)
-                return Response('Ok', status=200)
-                
+                return Response(chat_id, msg)
+
             else:
                 send_message(chat_id, 'Store Not Available')
-                return Response('Ok', status=200)   
+                return Response('Ok', status=200)
         
+
         else:
-            send_message(chat_id, 'Store ID is Wrong')
-            return Response('Ok', status=200)
+
+            send_mensage(chat_id, 'Store ID is Wrong')
+            return Response('OK', status=200)
+
 
     else:
         return '<h1> Rossmann Telegram BOT </h1>'
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
-    app.run( host='0.0.0.0', port=port)
-
-
-
-
-
+    app.run(host='0.0.0.0', port=port)
